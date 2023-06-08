c990a124bd4adf69d03ce7d67799dc0a44158ff5
Jazael Galindo <115960860+JazaeloG@users.noreply.github.com>
2023-06-08 10:40:29 -0600

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Register from './pages/register/Register'
-import NotFound from './pages/notFound/NotFound'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import NavBar from './components/NavBar'
 
 
